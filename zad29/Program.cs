ff008c37aacb93b5286ace84081e0058e7402f04
artemiy <karelskiy@mail.ru>
2023-04-07 21:39:11 +0300

    add zad29

---
+++ zad29/Program.cs
@@ -0,0 +1,14 @@
+﻿//Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+void FillArray(int[] newArray)
+{
+    Console.Write("Массив: ");
+    for (int i = 0; i < newArray.Length; i++)
+    {
+        newArray[i] = new Random().Next(1, 30);
+        Console.Write($"{newArray[i]}, ");
+    }
+}
+
+int[] array = new int[8];
+FillArray(array);
