ded78329b806bcc12fdcd47a46a7d5fbf25ba995
artemiy <karelskiy@mail.ru>
2023-04-07 21:37:57 +0300

    add zad27

---
+++ zad27/Program.cs
@@ -0,0 +1,16 @@
+﻿//Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+
+int SumOfDigits(int num)
+{
+int result=0;
+for(; num!= 0; num = num/10)
+{
+   result = result+num%10;
+}
+return result;
+}
+Console.Write("Введите число : ");
+int a = Convert.ToInt32(Console.ReadLine());
+int sum = SumOfDigits(a);
+Console.Write($"{a} => {sum}");
